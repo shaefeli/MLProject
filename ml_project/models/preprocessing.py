c8f743b12b236c6cb5c06aa40bfcacef3acfbf03
Simon <simon.haefeli@epfl.ch>
2017-10-04 21:15:06 +0200

    Implement Flatten class

---
+++ ml_project/models/preprocessing.py
@@ -0,0 +1,17 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_array
+
+class Flatten(BaseEstimator, TransformerMixin):
+	"""Flatten"""
+	def  __init__(self, dim=2):
+		self.dim = dim
+
+	def fit(self, X, y=None)
+		return self
+
+	def transform(self, X, y=None):
+		X = check_array(X)
+		X = X.reshape(-1, 176, 208, 176) # Bad practice: hard-coded dimensions
+		X = X.mean(axis=self.dim)
+		print('some stdout')
+		return X.reshape(X.shape[0], -1)
