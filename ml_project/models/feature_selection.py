907b28753583f4ac71b5c2677a05659de2924c8c
Simon <simon.haefeli@epfl.ch>
2017-10-08 14:36:46 +0200

    feature selection try

--- ml_project/models/feature_selection.py
+++ ml_project/models/feature_selection.py
@@ -2,6 +2,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils import check_random_state
 from sklearn.utils.validation import check_array, check_is_fitted
 from sklearn.utils.random import sample_without_replacement
+import numpy as np
 
 
 class RandomSelection(BaseEstimator, TransformerMixin):
@@ -20,12 +21,21 @@ class RandomSelection(BaseEstimator, TransformerMixin):
                             n_features,
                             self.n_components,
                             random_state=random_state)
+	
         return self
 
     def transform(self, X, y=None):
         check_is_fitted(self, ["components"])
         X = check_array(X)
         n_samples, n_features = X.shape
-        X_new = X[:, self.components]
+	
+	maxValue=X.max()
+	X_new =[]
+	for i in range (0,n_samples):
+		brain = X[i,:]
+		counts = np.histogram(brain, maxValue+1, (0,maxValue))
+		X_new.append(counts[0])
+
+        #X_new = X[:, self.components]
 
         return X_new
