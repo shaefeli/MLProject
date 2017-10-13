442489bca907af14006d2f14a05c5656777730d7
Simon <simon.haefeli@epfl.ch>
2017-10-13 15:06:04 +0200

    Cleaning feature_selection

--- ml_project/models/feature_selection.py
+++ ml_project/models/feature_selection.py
@@ -5,22 +5,22 @@ from sklearn.utils.random import sample_without_replacement
 import numpy as np
 
 
-class RandomSelection(BaseEstimator, TransformerMixin):
+class SimpleHistogram(BaseEstimator, TransformerMixin):
     """Random Selection of features"""
-    def __init__(self, n_components=1000, random_state=None):
-        self.n_components = n_components
+    def __init__(self, random_state=None):
+        #self.n_components = n_components
         self.random_state = random_state
-        self.components = None
+        #self.components = None
 
     def fit(self, X, y=None):
-        X = check_array(X)
-        n_samples, n_features = X.shape
+        #X = check_array(X)
+        #n_samples, n_features = X.shape
 
-        random_state = check_random_state(self.random_state)
-        self.components = sample_without_replacement(
-                            n_features,
-                            self.n_components,
-                            random_state=random_state)
+        #random_state = check_random_state(self.random_state)
+        #self.components = sample_without_replacement(
+        #                   n_features,
+        #                   self.n_components,
+        #                   random_state=random_state)
 	
         return self
 
