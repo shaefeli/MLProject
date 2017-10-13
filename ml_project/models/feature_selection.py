dc0ff013e3c0ceda2f1bb26cf8de0f8657ce4a5b
Simon <simon.haefeli@epfl.ch>
2017-10-13 15:19:05 +0200

    test

--- ml_project/models/feature_selection.py
+++ ml_project/models/feature_selection.py
@@ -7,21 +7,10 @@ import numpy as np
 
 class RandomSelection(BaseEstimator, TransformerMixin):
     """Random Selection of features"""
-    def __init__(self, n_components=1000, random_state=None):
+    def __init__(self, n_components=1000):
         self.n_components = n_components
-        self.random_state = random_state
-        self.components = None
 
     def fit(self, X, y=None):
-        X = check_array(X)
-        n_samples, n_features = X.shape
-
-        random_state = check_random_state(self.random_state)
-        self.components = sample_without_replacement(
-                            n_features,
-                            self.n_components,
-                            random_state=random_state)
-	
         return self
 
     def transform(self, X, y=None):
