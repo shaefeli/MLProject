8209803fc52b2a9634ee90be7944156c7bf78477
Simon <simon.haefeli@epfl.ch>
2017-10-13 16:02:16 +0200

    test

--- ml_project/models/feature_selection.py
+++ ml_project/models/feature_selection.py
@@ -5,35 +5,36 @@ from sklearn.utils.random import sample_without_replacement
 import numpy as np
 
 
-class RandomSelection(BaseEstimator, TransformerMixin):
-    """Random Selection of features"""
-    def __init__(self, n_components=1000, random_state=None):
-        self.n_components = n_components
+class HistogramSlice(BaseEstimator, TransformerMixin):
+    """Histogram of each slice"""
+    def __init__(self, sliceWidth=10, random_state=None):
+        self.sliceWidth = sliceWidth
         self.random_state = random_state
-        self.components = None
+        #self.components = None
 
     def fit(self, X, y=None):
         X = check_array(X)
         n_samples, n_features = X.shape
 
         random_state = check_random_state(self.random_state)
-        self.components = sample_without_replacement(
-                            n_features,
-                            self.n_components,
-                            random_state=random_state)
+#        self.components = sample_without_replacement(
+#                            n_features,
+#                            self.n_components,
+#                            random_state=random_state)
 	
         return self
 
     def transform(self, X, y=None):
-        check_is_fitted(self, ["components"])
+        #check_is_fitted(self, ["components"])
         X = check_array(X)
         n_samples, n_features = X.shape
 	
         maxValue=4420
         X_new =[]
+	nrBins = maxValue/sliceWidth
         for i in range (0,n_samples):
                 brain = X[i,:]
-                counts = np.histogram(brain, maxValue, (1,maxValue))
+                counts = np.histogram(brain, nrBins)
                 X_new.append(counts[0])
 
         #X_new = X[:, self.components]
