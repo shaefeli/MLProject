088b15837ff6b408620fbb355fd375e285a8856c
Simon <simon.haefeli@epfl.ch>
2017-10-18 17:49:35 +0200

    One of the final tests

--- ml_project/models/regression.py
+++ ml_project/models/regression.py
@@ -1,62 +1,68 @@
-import sklearn as skl
-import numpy as np
-import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.linear_model import RidgeCV
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
-from matplotlib import pyplot as plt
 
+class RidgeWithPost(BaseEstimator,TransformerMixin):
+    def __init__(self, weight=1.0):
+        self.ridge = RidgeCV(alpha=1.0)
+    def fit(self,X,y):
+        self.ridge.fit(X,y)
+        return self;
+    def predict(X):
+        return self.ridge.predict(X)
 
-class KernelEstimator(skl.base.BaseEstimator, skl.base.TransformerMixin):
-    """docstring"""
-    def __init__(self, save_path=None):
-        super(KernelEstimator, self).__init__()
-        self.save_path = save_path
+#class KernelEstimator(skl.base.BaseEstimator, skl.base.TransformerMixin):
+#    """docstring"""
+#    def __init__(self, save_path=None):
+#        super(KernelEstimator, self).__init__()
+#        self.save_path = save_path
 
-    def fit(self, X, y):
-        X, y = check_X_y(X, y)
-        self.y_mean = np.mean(y)
-        y -= self.y_mean
-        Xt = np.transpose(X)
-        cov = np.dot(X, Xt)
-        alpha, _, _, _ = np.linalg.lstsq(cov, y)
-        self.coef_ = np.dot(Xt, alpha)
+#    def fit(self, X, y):
+#        X, y = check_X_y(X, y)
+#        self.y_mean = np.mean(y)
+#        y -= self.y_mean
+#        Xt = np.transpose(X)
+#        cov = np.dot(X, Xt)
+#        alpha, _, _, _ = np.linalg.lstsq(cov, y)
+#        self.coef_ = np.dot(Xt, alpha)
 
-        if self.save_path is not None:
-            plt.figure()
-            plt.hist(self.coef_[np.where(self.coef_ != 0)], bins=50,
-                     normed=True)
-            plt.savefig(self.save_path + "KernelEstimatorCoef.png")
-            plt.close()
+#        if self.save_path is not None:
+#            plt.figure()
+#            plt.hist(self.coef_[np.where(self.coef_ != 0)], bins=50,
+#                     normed=True)
+#            plt.savefig(self.save_path + "KernelEstimatorCoef.png")
+#            plt.close()
 
-        return self
+#        return self
 
-    def predict(self, X):
-        check_is_fitted(self, ["coef_", "y_mean"])
-        X = check_array(X)
+#    def predict(self, X):
+#        check_is_fitted(self, ["coef_", "y_mean"])
+#        X = check_array(X)
 
-        prediction = np.dot(X, self.coef_) + self.y_mean
+#        prediction = np.dot(X, self.coef_) + self.y_mean
 
-        if self.save_path is not None:
-            plt.figure()
-            plt.plot(prediction, "o")
-            plt.savefig(self.save_path + "KernelEstimatorPrediction.png")
-            plt.close()
+#        if self.save_path is not None:
+#            plt.figure()
+#            plt.plot(prediction, "o")
+#            plt.savefig(self.save_path + "KernelEstimatorPrediction.png")
+#            plt.close()
 
-        return prediction
+#        return prediction
 
-    def score(self, X, y, sample_weight=None):
-        scores = (self.predict(X) - y)**2 / len(y)
-        score = np.sum(scores)
+#    def score(self, X, y, sample_weight=None):
+#        scores = (self.predict(X) - y)**2 / len(y)
+#        score = np.sum(scores)
 
-        if self.save_path is not None:
-            plt.figure()
-            plt.plot(scores, "o")
-            plt.savefig(self.save_path + "KernelEstimatorScore.png")
-            plt.close()
+#        if self.save_path is not None:
+#            plt.figure()
+#            plt.plot(scores, "o")
+#            plt.savefig(self.save_path + "KernelEstimatorScore.png")
+#            plt.close()
 
-            df = pd.DataFrame({"score": scores})
-            df.to_csv(self.save_path + "KernelEstimatorScore.csv")
+#            df = pd.DataFrame({"score": scores})
+#            df.to_csv(self.save_path + "KernelEstimatorScore.csv")
 
-        return score
+#        return score
 
-    def set_save_path(self, save_path):
-        self.save_path = save_path
+#    def set_save_path(self, save_path):
+#        self.save_path = save_path
