87503c4c37ce66e9cc9741387a71b8b5999f1421
Simon <simon.haefeli@epfl.ch>
2017-11-12 16:41:05 +0100

    Some debugging

--- ml_project/models/classification.py
+++ ml_project/models/classification.py
@@ -60,13 +60,14 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_array, check_is_fitted
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.svm import SVC
+from sklean.neural_network import MLPClassifier
 from scipy import stats
 import random
 import sys
 
 class LDAwithYHandling(BaseEstimator, TransformerMixin):
     def __init__(self):
-        self.lda=SVC(probability=True)
+        self.lda=MLPClassifier()
 
     def maxIndexWithSampling(y):
         chosenIndices = np.empty((y.shape[0],1))
